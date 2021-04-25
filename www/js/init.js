8cc5f73bfe960eaa829a7a052a2c4a004c2b5ee2
Khadidja IKHLEF <khadidja.ikhlef.mobile@gmail.com>
2021-04-25 13:49:09 +0200

    Ajout plugin camera et signature app

--- www/js/init.js
+++ www/js/init.js
@@ -13,8 +13,6 @@
 
   }); // end of document ready
  
- 
-
   $('.head-link').click(function (e) {
     e.preventDefault();
 
@@ -25,13 +23,5 @@
     }, 800);
   });
 
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-  ga('create', 'UA-60673008-2', 'auto');
-  ga('send', 'pageview');
 
 
